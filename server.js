e1615bf7922cea6022e2764dba2cf132769baece
Justin Vongdara <vongdara16@gmail.com>
2022-03-09 10:26:41 -0500

    create databas.js and update server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+import 'dotenv/config.js'
 import createError from 'http-errors'
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import logger from 'morgan'
+import methodOverride from 'method-override'
+
+import './config/database.js'
 
 // import routers
 import { router as indexRouter } from './routes/index.js'
