57eec40c6572a1391c3d124c2487783f222d1df6
Justin Vongdara <vongdara16@gmail.com>
2022-03-09 10:38:59 -0500

    complete step 3.1

--- controllers/books.js
+++ controllers/books.js
@@ -2,7 +2,9 @@ import { Book } from "../models/book.js";
 
 function index(req, res){
   console.log('book index view')
-  res.render('books/index')
+  res.render('books/index', {
+    title: 'Unit 2 Assesment'
+  })
 }
 
 export{
