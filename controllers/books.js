2b7f0d252a47e36d5af9dab5ea703fa92bb15a92
Justin Vongdara <vongdara16@gmail.com>
2022-03-09 10:43:19 -0500

    complete step 3.4

--- controllers/books.js
+++ controllers/books.js
@@ -2,8 +2,16 @@ import { Book } from "../models/book.js";
 
 function index(req, res){
   console.log('book index view')
-  res.render('books/index', {
-    title: 'Unit 2 Assesment'
+  Book.find({})
+  .then(books => {
+    res.render('books/index', {
+      title: 'Unit 2 Assesment',
+      books,
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/books')
   })
 }
 
